01ac77ce906ee004e1833de4816213f88889c678
MarekVilimovsky <129778878+MarekVilimovsky@users.noreply.github.com>
2023-06-06 21:25:17 +0200

    Add files via upload

---
+++ ThreadTester-main/ThreadTester/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ThreadTester
+{
+    public partial class Form1 : Form
+    {
+        // nový timer
+        clsTimer mobjTimer;
+        int mintPocitadlo = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // start timeru
+            mobjTimer = new clsTimer();
+            mobjTimer.Tick += clsTimer_tick;
+            mobjTimer.mobjForm = this;
+            mobjTimer.Start();
+        }
+
+
+        //-------------------------------------
+        // tick timeru
+        //-------------------------------------
+        void clsTimer_tick()
+        {
+            mintPocitadlo++;
+            textBox1.Text = mintPocitadlo.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mobjTimer.Stop();
+        }
+    }
+}
---
+++ ThreadTester-main/ThreadTester/clsTimer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTester
+{
+    internal class clsTimer
+    {
+        // interval v us
+        int mintIntervalIn_us;
+
+        //Timespan
+        TimeSpan mtsTimeSpan;
+
+        // thread pro časování timeru
+        Thread mobjVlákno1;
+
+        // zastavit thread
+        bool mblStop;
+
+        /// deklarace ukazatele na funkci - "public delegate void ..."
+        // definice tvaru eventu
+        public delegate void dlgTick();
+        // deklarace eventu ticku
+        public event dlgTick Tick;
+
+        // formulář kde existuje muj objekt
+        public Form1 mobjForm; 
+
+        //-------------------------------------
+        // interval tick timeru
+        //-------------------------------------
+        public int Interval
+        {
+            get { return mintIntervalIn_us; }
+            set { mintIntervalIn_us = value; }
+        }
+        
+        //-------------------------------------
+        // konstruktor timeru
+        //-------------------------------------
+        public clsTimer() 
+        {
+            mtsTimeSpan= TimeSpan.FromTicks(10000000);
+
+            mobjForm = null;
+        }
+
+        //-------------------------------------
+        // start timeru
+        //-------------------------------------
+        public bool Start()
+        {
+            // vytvoření threadu
+            mobjVlákno1 = new Thread(MojeRutina);
+
+            //spustit thread
+            mblStop = false;
+            mobjVlákno1.Start();
+
+            return true;
+        }
+
+        //-------------------------------------
+        // thread timeru
+        //-------------------------------------
+        private void MojeRutina()
+        {
+            do
+            {
+                {
+                    // zavolat eventy v threadu formuláře
+                    if (mobjForm!=null)
+                        mobjForm.Invoke(Tick);
+
+                    // pozastavit thread
+                    Thread.Sleep(mtsTimeSpan);
+                    // do{ } while ();
+                }
+            } while (mblStop == false);
+        }
+
+        //-------------------------------------
+        // stop timeru
+        //-------------------------------------
+        public bool Stop()
+        {
+            mobjForm = null;
+            
+            // stop thread
+            mblStop = true;
+
+            return true;
+        }
+    }
+}
